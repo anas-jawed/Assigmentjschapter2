727c7b6de8ca476685914db9843e243dfc86f708
Anas Jawed <anasjawedbmj@gmail.com>
2024-10-14 11:30:15 +0300

    chapter2javascriptassigment

---
+++ app.js
@@ -0,0 +1,67 @@
+
+// 1 .Declare a variable called username
+
+// var username;
+
+// 2. Declare a variable called myName & assign to it a string 
+// that represents your Full Name.
+
+
+// let myName = "Anas Jawaid";
+
+// 3 .Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+// let message = "Hello World";
+// alert(message);
+
+//  4. Write a script to save student’s bio data in JS variables and 
+// show the data in alert boxes
+
+// let  myName = "Jhone Doe";
+// let myAge = "15 years old";
+// let info = "Certified Mobile Application Devolopment";
+
+// alert(myName);
+// alert(myAge);
+// alert(info);
+
+// 5. Write a script to display the following alert using one JS 
+// variable:
+
+// let message = "PIZZA,\n PIZZ,\n PIZ,\n PI,\n P ";
+// alert(message);
+
+// 6. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box
+
+
+// let email = "anasjawed@gmail.com";
+// alert("My email address is" + "" + "" + "" + email);
+
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box
+
+// let book = "A smarter way to learn javascript";
+
+// alert("I am trying to learn from the book" + "" + "" + book);
+
+// 8. Write a script to display this in browser through JS
+
+// let message = "Yah! I can write HTML content through Javascript";
+
+// document.write(message);
+
+//  9 .// Store following string in a variable and show in alert and 
+// browser through JS
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+
+// let message = "Hello World";
+
+// alert(message);
+// document.write(message);
